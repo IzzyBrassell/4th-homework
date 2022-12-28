d282f40ca15dd5389d69161a6af5dbe49283dadd
Israel Brassell <israel.brassell@gmail.com>
2022-12-27 19:45:16 -0500

    Working quiz

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ class Question {
     }
 }
 
+
 const question1 = new Question (
     'Which would be the correct way to set a boolean in Javascript',
     ['var x = "true"', 'x = TRUE', 'var x = "false"', 'var x = false'],
@@ -53,3 +54,65 @@ const question8 = new Question (
     ['2 > 5', 'true = false', 'let x = true console.log(x)', 'var name = name'],
     'let x = true console.log(x)'
 )
+
+class Quiz {
+    constructor(questions) {
+      this.questions = questions;
+      this.currentIndex = 0;
+    }
+  
+    getCurrentQuestion() {
+      return this.questions[this.currentIndex];
+    }
+  
+    nextQuestion() {
+      this.currentIndex++;
+    }
+  
+    hasEnded() {
+      return this.currentIndex >= this.questions.length;
+    }
+  }
+   
+  let quiz = new Quiz([question1, question2, question3, question4, question5, question6, question7, question8])
+
+  function showCurrentQuestion() {
+    const question = quiz.getCurrentQuestion();
+    const questionElement = document.getElementById("question");
+    questionElement.innerText = question.question;
+  
+    const choices = question.choices;
+    for (let i = 0; i < choices.length; i++) {
+      const choiceElement = document.getElementById(`choice${i}`);
+      choiceElement.innerText = choices[i];
+    }
+  }
+  
+  let score = 0;
+
+  function checkAnswer(selectedChoice) {
+    if (selectedChoice === quiz.getCurrentQuestion().answer) {
+      score += 12.5;
+    } else {
+      score -= 12.5;
+    }
+    showNextQuestion();
+    updateScore();
+  }
+  
+  function updateScore() {
+    const scoreElement = document.getElementById("score");
+    scoreElement.innerText = `Score: ${score}`;
+  }
+  
+  function showNextQuestion() {
+    quiz.nextQuestion();
+    if (quiz.hasEnded()) {
+      alert("Quiz has ended!");
+      return;
+    }
+    showCurrentQuestion();
+  }
+  
+  showCurrentQuestion();
+
